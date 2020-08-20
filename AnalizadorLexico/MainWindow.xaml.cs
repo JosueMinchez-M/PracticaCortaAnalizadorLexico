5bcc84baec356e208efc7edfdb8b7b5dcd9ab528
JosueMinchez-M <josueminchezm@gmail.com>
2020-08-20 04:10:22 -0600

    Se agrega la logica funcional para determinar si es un numero entero o numero decimal, asi como se captura su tipo de moneda ya sea en Q o $

--- AnalizadorLexico/MainWindow.xaml.cs
+++ AnalizadorLexico/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows;
 
@@ -30,12 +31,37 @@ namespace AnalizadorLexico
                 if (Regex.IsMatch(lexema[i], @"^[a-zA-Z]+$"))
                 {
                     listV_palabra.Items.Add(lexema[i]);
+                    
                 }else if (Regex.IsMatch(lexema[i], @"^[0-9]+$"))
                 {
                     listV_numEntero.Items.Add(lexema[i]);
+                }else if (Regex.IsMatch(lexema[i], "[Q$]+.*"))
+                {
+                    Console.WriteLine(lexema[i].Substring(0,1));
+                    String moneda = lexema[i].Substring(0, 1);
+                    Console.WriteLine(lexema[i].Substring(1, lexema[i].Length-1));
+                    String numero = lexema[i].Substring(1, lexema[i].Length - 1);
+                    if (Regex.IsMatch(moneda, "[Q$]+.*"))
+                    {
+                        listV_moneda.Items.Add(moneda);
+                    }
+                    if (Regex.IsMatch(numero, ".*[.].*" + "[0-9]*"))
+                    {
+                        //String numeroEntero = numero.Substring(0, numero.IndexOf("."));
+                        listV_numDecimal.Items.Add(numero);
+                    } 
+                    if (Regex.IsMatch(numero, @"^[0-9]+$"))
+                    {
+                        listV_numEntero.Items.Add(numero);
+                    }
+                    //listV_numEntero.Items.Add(lexema[i]);
+                }else if (Regex.IsMatch(lexema[i], ".*[.].*" + "[0-9]*"))
+                {
+                    listV_numDecimal.Items.Add(lexema[i]);
                 }
                 //System.Console.WriteLine(lexema[i]);
             }
+
 //            foreach (var lexema in lexema)
 //            {
 //                System.Console.WriteLine($"<{lexema}>");
